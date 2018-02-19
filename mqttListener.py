91ba814effa6b9bdc08911e9597cd3a9c749bf43
Ben Clouser <bclouse91@gmail.com>
2018-02-18 17:04:39 -0800

    Adding mqttListener and modifying broker ip address

--- mqttListener.py
+++ mqttListener.py
@@ -9,7 +9,8 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("#")
+    client.subscribe("/#")
+    #client.subscribe("/snowden/bensApartment/bedroom/windowShade/status")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -19,16 +20,14 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
+# For testing
+#client.connect("test.mosquitto.org", 1883, 60)
 client.connect("192.168.1.199", 1883, 60)
 
-print "publishing message"
-ret = client.publish("/bensRoom/hdmiSelector1", '{"command":"hdmi1"}')
 
-print "Publishe status = " + str(ret[0])
-
-client.disconnect()
+#client.disconnect()
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-#client.loop_forever()
+client.loop_forever()
